48aa226565ccc3d5b47c185c6e2f270bc44ee877
sangiba <sangiba@baleomol.dev>
2023-03-17 15:48:14 +0700

    Fix Responsive

--- src/Profile.js
+++ src/Profile.js
@@ -6,7 +6,15 @@ function Profile() {
         <div>
             <Detail
                 img='https://planetanft.com/wp-content/uploads/2021/12/Doodles-1.png'
-                name='Alex John'
+                name='Bambang Gentolet'
+                jobs='Fullstack Developers'
+                desc='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+                link1='#'
+                link2='#'
+            />
+            <Detail
+                img='https://planetanft.com/wp-content/uploads/2021/12/Doodles-1.png'
+                name='Bambang Gentolet'
                 jobs='Fullstack Developers'
                 desc='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
                 link1='#'
@@ -19,8 +27,8 @@ function Profile() {
 function Detail(props) {
     const { img, name, jobs, desc, link1, link2 } = props;
     return (
-        <div className='p-0 md:p-11 h-screen'>
-            <div className="container mx-auto my-auto bg-gray-700 md:rounded-xl md:shadow lg:border p-8 m-10">
+        <div className='p-0 sm:p-11 h-screen'>
+            <div className="container mx-auto my-auto bg-gray-700 sm:rounded-xl sm:shadow sm:border p-8 m-10">
                 <div className='object-cover bg-gray-50 w-64 h-64 rounded-full mx-auto mb-5 mt-5 shadow-xl'>
                     <img className='object-cover h-64 w-64 rounded-full mx-auto p-2' src={img}></img>
                 </div>
